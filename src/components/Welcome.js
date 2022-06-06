c8cc5d6ace2a81e5d451a2b591c8eccc1071e9dd
jasnlink <chem9310@gmail.com>
2022-06-06 05:52:50 -0400

    Create Welcome.js

---
+++ src/components/Welcome.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+
+import './Welcome.css';
+
+import PasscodeKeypad from './Forms/PasscodeKeypad'
+import PasscodeDisplay from './Forms/PasscodeDisplay'
+
+function Welcome({ setStep }) {
+	
+	const [passcode, setPasscode] = useState(6969)
+	const [passcodeInput, setPasscodeInput] = useState('')
+
+	const [valid, setValid] = useState(false)
+
+	useEffect(() => {
+
+		if(valid === true) {
+			setStep(2)
+		}
+
+	}, [valid])
+
+	return (
+		<>
+			<div className="container-fluid py-4">
+				<PasscodeDisplay 
+					passcode={passcode} 
+					passcodeInput={passcodeInput}
+				/>
+				<PasscodeKeypad 
+					passcode={passcode} 
+					passcodeInput={passcodeInput} 
+					setPasscodeInput={(pass) => setPasscodeInput(pass)} 
+					setValid={(valid) => setValid(valid)}
+				/>
+			</div>
+		</>
+	)
+
+
+}
+
+export default Welcome;

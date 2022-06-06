dad16f608ee54a9c3a5e342e13761fdd915d422b
jasnlink <chem9310@gmail.com>
2022-06-06 05:55:13 -0400

    Implement header bar clock

---
+++ src/components/HeaderBar.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+import './HeaderBar.css'
+
+
+function HeaderBar({ date, time }) {
+
+	return (
+
+		<div className="container-fluid main">
+			<div className="row justify-content-center">
+				<div className="col-2 text-center">
+					<p className="h4 time-display">{time}</p>
+				</div>
+				<div className="col-3 text-start">
+					<p className="h4 date-display">{date}</p>
+				</div>
+			</div>
+		</div>
+
+	)
+
+}
+export default HeaderBar

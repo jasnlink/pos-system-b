7cb7d71f950519c4eb3e993927bce717747a4f68
jasnlink <chem9310@gmail.com>
2022-06-08 09:27:23 -0400

    Added Loading screen

---
+++ src/components/Preface.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Preface.css'
+
+
+function Preface() {
+
+	return (
+
+		<div className="preface-main display-1">
+			LOGO HERE
+		</div>
+	)
+
+}
+export default Preface

d9dd7adf7737ec5bf0b5618ed556669b48143de0
jasnlink <chem9310@gmail.com>
2022-06-13 03:52:42 -0400

    Head styling

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -8,13 +8,21 @@ function HeaderBar({ date, time }) {
 	return (
 
 		<div className="container-fluid headerbar-main">
-			<div className="row justify-content-center">
-				<div className="col-2 text-center">
-					<p className="h4 time-display">{time}</p>
+			<div className="headerbar-item">
+				<p className="h4 time-display">LOGO HERE</p>
+			</div>
+			<div className="headerbar-item">
+				<div className="headerbar-item-time">
+					<div className="text-center">
+						<p className="h4 time-display">{time}</p>
+					</div>
+					<div className="text-start">
+						<p className="h4 date-display">{date}</p>
+					</div>
 				</div>
-				<div className="col-3 text-start">
-					<p className="h4 date-display">{date}</p>
-				</div>
+			</div>
+			<div className="headerbar-item">
+				
 			</div>
 		</div>
 

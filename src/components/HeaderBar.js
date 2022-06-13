2d1607748edc30c0a833641f807dfc2696bea850
jasnlink <chem9310@gmail.com>
2022-06-13 11:30:40 -0400

    change header styles

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -9,13 +9,13 @@ function HeaderBar({ date, time }) {
 
 		<div className="container-fluid headerbar-main">
 			<div className="headerbar-item">
-				<p className="h4 time-display">LOGO HERE</p>
+				<p className="h4">LOGO HERE</p>
 			</div>
 			<div className="headerbar-item">
 				<div className="headerbar-item-time">
-					<div className="text-center">
-						<p className="h4 time-display">{time}</p>
-					</div>
+					
+						<p className="time-display text-monospace">{time}</p>
+					
 					<div className="text-start">
 						<p className="h4 date-display">{date}</p>
 					</div>
